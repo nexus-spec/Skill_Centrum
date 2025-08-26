58c698f753f6302e994022985b83529fd9d3ae44
nexus-spec <jakinorymnexus@gmail.com>
2025-08-26 16:06:27 +0100

    Add files via upload

---
+++ education query code.sql
@@ -0,0 +1,16 @@
+CREATE TABLE educations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    school VARCHAR(150) NOT NULL,
+    degree VARCHAR(100) NOT NULL,
+    fieldOfStudy VARCHAR(150) NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NULL,
+    present BOOLEAN DEFAULT FALSE,
+    applicantId INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_educations_applicant 
+    FOREIGN KEY (applicantId) 
+    REFERENCES applicants(id) 
+    ON DELETE CASCADE
+);
